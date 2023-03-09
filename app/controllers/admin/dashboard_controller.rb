f1634a0fe0e007886505d7b157af6d448366057a
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-03-08 17:19:00 -0800

    basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+
+def authenticate
+  authenticate_or_request_with_http_basic do |username, password|
+   username == ENV["USERNAME"] && password == ENV["PASSWORD"]
+  end
+end
   def show
   end
 end
